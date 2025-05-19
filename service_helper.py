07c672044665305bbbe927c864916f1efab13f37
John Martinsson <john.martinsson@ri.se>
2025-05-19 11:23:18 +0200

    Adds settings

--- service_helper.py
+++ service_helper.py
@@ -61,8 +61,8 @@ Wants=network-online.target
 Type=simple
 Restart=always
 RestartSec=5
-User=dsp
-Group=dsp
+User=bsp
+Group=bsp
 ExecStart=/usr/bin/python3 {script_path} \
           --ip {ip} --station {station} --user {user} --password {password} \
           --segment_time {segment_time} --loglevel {loglevel} \
@@ -80,8 +80,8 @@ Wants=network-online.target
 
 [Service]
 Type=oneshot
-User=dsp
-Group=dsp
+User=bsp
+Group=bsp
 ExecStart=/usr/bin/python3 {exec_path} --config {config_path}
 
 [Install]
