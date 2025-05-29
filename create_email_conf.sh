dea8e6a13f6d40e7b3832aa710aa873b34385fa7
John Martinsson <john.martinsson@ri.se>
2025-05-29 15:15:54 +0200

    Adds all emails.

--- create_email_conf.sh
+++ create_email_conf.sh
@@ -3,7 +3,7 @@ SMTP_HOST=smtp.gmail.com
 SMTP_PORT=587
 SMTP_USER=auklab2025@gmail.com
 SMTP_PASS=app-password-or-smtp-token
-ALERT_TO=john.martinsson@ri.se,john.martinsson@gmail.com
+ALERT_TO=john.martinsson@ri.se,john.martinsson@gmail.com,jonas.sundberg@slu.se,delia.fano.yela@ri.se,olof.mogren@ri.se
 EOF
 sudo chmod 600 /etc/monitor_email.conf
 sudo chown root:root /etc/monitor_email.conf
